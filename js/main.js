6f7c9b60b53907ab737192a7d4569ecc6fd74bff
milk9111 <connorlndbrg349@gmail.com>
2017-06-16 00:08:59 -0700

    trying to merge

--- js/main.js
+++ js/main.js
@@ -5,21 +5,50 @@ var paddingOffset = 0;
 $(document).ready(function(){
 	addLineNum();
     $("#input").keydown(function(e) {
-	   var code = e.keyCode ? e.keyCode : e.which;
-	   if (code == 13) {  // Enter keycode
-	 	   addLineNum();
-	   } 
+    	var editableID = document.getElementById("input");
+	    var code = e.keyCode ? e.keyCode : e.which;
+	    console.log("the code is " + code);
+	    console.log("the preceding char is " + getCharacterPrecedingCaret(editableID).charCodeAt(0));
+	    if (code == 13) {  // Enter keycode
+	 		addLineNum();
+	    } else if (code == 8 && getCharacterPrecedingCaret(editableID) == 13) {
+	    	console.log("in here");
+	    	remLineNum();
+
+	    }
 	});
 }); 
 
 
+function getCharacterPrecedingCaret(containerEl) {
+    var precedingChar = "", sel, range, precedingRange;
+    if (window.getSelection) {
+        sel = window.getSelection();
+        if (sel.rangeCount > 0) {
+            range = sel.getRangeAt(0).cloneRange();
+            range.collapse(true);
+            range.setStart(containerEl, 0);
+            precedingChar = range.toString().slice(-1);
+        }
+    } else if ( (sel = document.selection) && sel.type != "Control") {
+        range = sel.createRange();
+        precedingRange = range.duplicate();
+        precedingRange.moveToElementText(containerEl);
+        precedingRange.setEndPoint("EndToStart", range);
+        precedingChar = precedingRange.text.slice(-1);
+    }
+    return precedingChar;
+}
+
+
 function addLineNum() {
-	//console.log("before appending");
-	//$("#line-nums").append('<div><label style="font-size: 75%;margin-bottom: 0px;' 
-	//	+ 'padding-top: ' + (0 - paddingOffset) + 'px;">' + lineCount + '</label></div>');
-	$("#line-nums").append("<div>" + lineCount + "</div>");
-	paddingOffset+=10;
+	$("#line-nums").append('<div id="' + lineCount + '">' + lineCount + "</div>");
 	lineCount++;
+}
+
+function remLineNum() {
+	$("#line-nums").remove('<div id="' + lineCount + '">' + lineCount + "</div>");
+	lineCount--;
 }
 
 function saveText() {
